79827c59919fc8dfec2e7f7a0decc62f4eb6683d
Sh@nto <shanto57575@gmail.com>
2023-11-05 22:19:38 +0600

    Create Factorial_Num_less_Than_equal_N.cpp

---
+++ Factorial_Num_less_Than_equal_N.cpp
@@ -0,0 +1,13 @@
+// problem : https://www.codingninjas.com/studio/problems/factorial-numbers-not-greater-than-n_8365435?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+void fact(vector<long long> &v, long long n, long long x) {
+    if(x*v.back()>n)return;
+    v.push_back(x*v.back());
+    fact(v,n,x+1);
+}
+
+vector<long long> factorialNumbers(long long n) {
+    vector<long long>v{1};
+    fact(v,n,2);
+    return v;
+}
