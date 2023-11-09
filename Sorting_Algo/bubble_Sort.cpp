10e2c4b311e88e4481620539ecaf4a556d3b9ffd
Sh@nto <shanto57575@gmail.com>
2023-11-09 16:49:02 +0600

    Update bubble_Sort.cpp

--- Sorting_Algo/bubble_Sort.cpp
+++ Sorting_Algo/bubble_Sort.cpp
@@ -2,11 +2,18 @@
 
 void bubbleSort(int arr[], int n) 
 {
-    for(int i=0; i<=n-2; i++){
-        for(int j=0; j<=n-i-2; j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+    for (int i = 0; i <= n - 2; i++)
+    {
+        bool swapped = 0;
+        for (int j = 0; j <= n - i - 2; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = 1;
             }
         }
+        if (swapped == 0)
+            break;
     }
 }
