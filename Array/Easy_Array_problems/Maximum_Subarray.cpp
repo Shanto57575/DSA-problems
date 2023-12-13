921ce3f47e9f6d1350a56969f1d7113e1aa570c2
Sh@nto <shanto57575@gmail.com>
2023-12-13 20:37:05 +0600

    Update Maximum_Subarray.cpp

--- Array/Easy_Array_problems/Maximum_Subarray.cpp
+++ Array/Easy_Array_problems/Maximum_Subarray.cpp
@@ -14,3 +14,26 @@ public:
         return maximum;
     }
 };
+
+// Follow uo question : if we want to know the which subarray guves the maximum  number
+  long long maximum = INT_MIN;
+  long long sum = 0;
+  int firstIndex = -1, lastIndex = -1;
+  int start;
+
+  for (int i = 0; i < nums.size(); i++)
+  {
+    if (sum < 0)
+    {
+      sum = 0;
+      start = i;
+    }
+    sum += nums[i];
+    if (sum > maximum)
+    {
+      maximum = sum;
+      firstIndex = start;
+      lastIndex = i;
+    }
+  }
+
