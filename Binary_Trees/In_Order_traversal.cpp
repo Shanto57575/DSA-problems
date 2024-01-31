4525b2f391a245989bd19537174e61f8944664b0
Sh@nto <shanto57575@gmail.com>
2024-01-31 15:34:32 +0600

    Update In_Order_traversal.cpp

--- Binary_Trees/In_Order_traversal.cpp
+++ Binary_Trees/In_Order_traversal.cpp
@@ -12,6 +12,14 @@
  * };
  */
 
+// For both and space time and space are same
+
+// tc ----> O(N)
+// Sc ----> O(N)
+
+// ----------------------------------------------------               ----------------------------------------------------------------------
+                                                         Recursive way
+// -----------------------------------------------------              ----------------------------------------------------------------------
 class Solution {
 public:
     vector<int>v;
@@ -20,7 +28,39 @@ public:
         inorderTraversal(root->left);
         v.push_back(root->val);
         inorderTraversal(root->right);
-
         return v;
+    }
+};
+
+// ----------------------------------------------------               ----------------------------------------------------------------------
+                                                         Iterative way
+// -----------------------------------------------------              ----------------------------------------------------------------------
+
+// Using Stack
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        TreeNode *node = root;
+        vector<int>ans;
+
+        while(1)
+        {
+            if(node != NULL)
+            {
+                st.push(node);
+                node = node->left;
+            }
+            else
+            {
+                if(st.empty()) break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
+        return ans;
     }
 };
