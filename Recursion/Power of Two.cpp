d08aaa28e82793bb93fc7749f01aa4c2437615ab
Sh@nto <shanto57575@gmail.com>
2024-02-03 19:18:12 +0600

    Create Power of Two.cpp

---
+++ Recursion/Power of Two.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/power-of-two/description/
+
+// tc--> O(log N)
+// sc--> O(1)
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1)return true;
+        long long ans = 1;
+        while(1)
+        {
+            ans*=2;
+            if(ans==n)return true;
+            if(ans>n)return false;
+        }
+        return false;
+    }
+};
+// recursive solution
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1)return true;
+        if(n%2!=0 || n<=0)return false;
+        return isPowerOfTwo(n/2);
+    }
+};
+
+// tc--> O(log N)
+// sc--> O(1)
