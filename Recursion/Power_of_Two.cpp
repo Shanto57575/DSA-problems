6feff2d30931a7abd37769eac8a45e3da00e603e
Sh@nto <shanto57575@gmail.com>
2024-02-03 19:31:03 +0600

    Update and rename Power of Two.cpp to Power_of_Two.cpp

--- Recursion/Power_of_Two.cpp
+++ Recursion/Power_of_Two.cpp
@@ -28,3 +28,15 @@ public:
 
 // tc--> O(log N)
 // sc--> O(1)
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)return false;
+        return ((n & (n-1)) == 0);
+        return false;
+    }
+};
+
+// tc --> O(1)
+// sc --> O(1)
