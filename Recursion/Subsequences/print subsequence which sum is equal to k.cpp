ee9b76685fc5fdb5ad6d052440613a19fd81faf5
Sh@nto <shanto57575@gmail.com>
2024-02-13 17:22:03 +0600

    Create print subsequence which sum is equal to k.cpp

---
+++ Recursion/Subsequences/print subsequence which sum is equal to k.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// print subsequence which sum is equal to k;
+
+void printSeq(int index, vector<int> v, int arr[], int n, int sum, int k)
+{
+    if (index >= n)
+    {
+        if (sum == k)
+        {
+            for (auto i : v)
+                cout << i << " ";
+            cout << endl;
+        }
+        return;
+    }
+
+    v.push_back(arr[index]);
+    sum += arr[index];
+    printSeq(index + 1, v, arr, n, sum, k);
+    v.pop_back();
+    sum -= arr[index];
+    printSeq(index + 1, v, arr, n, sum, k);
+}
+
+int main()
+{
+    int n = 3;
+    int arr[n] = {3, 1, 2};
+    int k = 3;
+    vector<int> v;
+    printSeq(0, v, arr, n, 0, k);
+}
