9221ec1e9e1e9940c1d6b0e998352d1325535b46
Sh@nto <shanto57575@gmail.com>
2024-03-14 13:13:44 +0600

    Create Assign Cookies.cpp

---
+++ Greedy/Assign Cookies.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/assign-cookies/description/
+
+// tc --> O(n log n)
+// sc --> O(1)
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int child = 0;
+        int cookies = 0;
+
+        while(child < g.size() && cookies < s.size())
+        {
+           if(s[cookies] >= g[child])
+           {
+             child++;
+           }
+           cookies++;
+        }
+
+        return child;
+    }
+};
